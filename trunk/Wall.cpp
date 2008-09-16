1ce1d148f0333a81048c38893d2da0edca3e231a
dgoemans <dgoemans@aaf11430-8420-11dd-9ffd-cdf95c2cae98>
2008-09-16 19:31:35 +0000

    fixed wall bug, time for level making

--- trunk/Wall.cpp
+++ trunk/Wall.cpp
@@ -31,6 +31,32 @@ bool Wall::Intersect( const Vector3f& position, float radius )
     Vector3f delta( PARTICLE_SIZE/2, PARTICLE_SIZE/2, PARTICLE_SIZE/2 );
 
     // NEED RANGE CHECKING!!!
+    float d_start = position.distanceSquared( m_startPosition );
+    float d_end = position.distanceSquared( m_endPosition );
+
+    if( d_start < d_end )
+    {
+        Vector3f startToEnd = m_endPosition - m_startPosition;
+        startToEnd.normalise();
+
+        Vector3f startToPos = position - m_startPosition;
+        startToPos.normalise();
+
+        if( startToEnd.dot( startToPos ) < 0 )// && startToPos.lengthSquared() > (PARTICLE_SIZE/2)*(PARTICLE_SIZE/2) )
+            return false;
+    }
+    else if( d_end < d_start )
+    {
+        Vector3f startToEnd = m_startPosition - m_endPosition;
+        startToEnd.normalise();
+
+        Vector3f startToPos = position - m_endPosition;
+        startToPos.normalise();
+
+        if( startToEnd.dot( startToPos ) < 0 ) //&& startToPos.lengthSquared() > (PARTICLE_SIZE/2)*(PARTICLE_SIZE/2) )
+            return false;
+    }
+
 
     Vector3f dist = m_endPosition - m_startPosition;
     a = dist.dot( dist );
