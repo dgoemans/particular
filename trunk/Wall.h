7220b2a9b382c3145ae94f54b768ff314b4bf0e8
dgoemans <dgoemans@aaf11430-8420-11dd-9ffd-cdf95c2cae98>
2008-09-16 19:02:35 +0000

    Added initial files

---
+++ trunk/Wall.h
@@ -0,0 +1,22 @@
+#ifndef WALL_H_INCLUDED
+#define WALL_H_INCLUDED
+
+#include <vmmlib/vmmlib.h>
+
+using namespace vmml;
+
+class Wall
+{
+
+public:
+    Wall();
+    Wall( const Vector3f& start, const Vector3f& end );
+    virtual ~Wall();
+    bool Intersect( const Vector3f& position, float radius );
+    Vector3f m_startPosition;
+    Vector3f m_endPosition;
+    Vector3f m_color;
+    bool m_locked;
+};
+
+#endif // WALL_H_INCLUDED
